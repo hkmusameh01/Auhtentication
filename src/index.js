aa758050c882dba913453ee106ff7c469e70987a
hkmusameh01 <abdalhakem1226@gmail.com>
2022-09-14 11:21:33 +0300

    process.env.PORT

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-app.set('PORT', 6060)
+app.set('PORT', process.env.PORT || 6060)
 
 app.listen(app.get('PORT'), () => {
   console.log(`Server is running on http://localhost:${app.get('PORT')}`);
